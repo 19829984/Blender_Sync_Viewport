35494843e7331834e5126d62c67461bf631bba81
19829984 <57331630+19829984@users.noreply.github.com>
2023-04-07 01:54:57 -0400

    Remove unused code

--- utils/registration.py
+++ utils/registration.py
@@ -10,11 +10,3 @@ def register_classes(classes):
 def unregister_classes(classes):
     for cls in classes[::-1]:
         bpy.utils.unregister_class(cls)
-
-# def register_icons(paths):
-#     global custom_icons
-#     custom_icons = bpy.utils.previews.new()
-#     icons_dir = os.path.join(os.path.dirname(__file__), "icons")
-
-#     for path in paths:
-#         custom_icons.load()
